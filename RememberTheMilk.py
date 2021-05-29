1dcf7ec9fcfe2b64a2af5347bfab9a5616ca1049
agnieszkagi <agnieszkagi@users.noreply.github.com>
2021-05-29 13:35:55 +0000

    Added 3rd test case

--- RememberTheMilk.py
+++ RememberTheMilk.py
@@ -2,15 +2,18 @@ import unittest
 import os
 
 from appium import webdriver
-from time import sleep
+from locators import GeneralLocators
 from locators import RegisterPageLocators
+from locators import LoginPageLocators
+from locators import HomePageLocators
+
 
 PATH = lambda p: os.path.abspath(
     os.path.join(os.path.dirname(__file__), p)
 )
 
 
-class Test2Appium(unittest.TestCase):
+class TestRememberTheMilk(unittest.TestCase):
     def setUp(self):
         desired_caps = {}
         desired_caps['app'] = PATH('RememberTheMilk.apk')
@@ -25,56 +28,83 @@ class Test2Appium(unittest.TestCase):
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
         self.driver.implicitly_wait(2)
 
+
     def tearDown(self):
         self.driver.quit()
 
-    def testRegistrationWithWronEmail(self):
-        self.driver.is_app_installed('com.rememberthemilk.MobileRTM')
+    '''
+    def test1RegistrationWithWronEmail(self):
+        self.driver.is_app_installed(GeneralLocators.REMEMBER_THE_MILK_APP)
         #click skip button if displayed
-        list = self.driver.find_elements_by_id('com.rememberthemilk.MobileRTM:id/alert_generic_notice')
+        list = self.driver.find_elements_by_id(GeneralLocators.SKIPP_BUTTON)
         if len(list)>0:
             list[0].click()
-        #if self.driver.find_element_by_id('com.rememberthemilk.MobileRTM:id/alert_generic_notice'):
-        #    self.driver.find_element_by_id('com.rememberthemilk.MobileRTM:id/alert_generic_notice').click()
 
-        #click register button
-        #self.driver.find_element_by_id('com.rememberthemilk.MobileRTM:id/sign_button').click()
+        #click sign in button
         el = self.driver.find_element_by_id(RegisterPageLocators.SING_IN_BUTTON)
         el.click()
         #Data input
-        self.driver.find_element_by_id('com.rememberthemilk.MobileRTM:id/firstname_field').send_keys("Agnieszka")
-        self.driver.find_element_by_id('com.rememberthemilk.MobileRTM:id/lastname_field').send_keys("Nowak")
-        self.driver.find_element_by_id('com.rememberthemilk.MobileRTM:id/email_field').send_keys("anowak.com")
-        self.driver.find_element_by_id('com.rememberthemilk.MobileRTM:id/username_field').send_keys("testernowak")
-        self.driver.find_element_by_id('com.rememberthemilk.MobileRTM:id/password_field').send_keys("passswwwo")
-        self.driver.find_element_by_class_name('android.widget.Button').click()
-        sleep(2)
+        el = self.driver.find_element_by_id(RegisterPageLocators.NAME_INPUT)
+        el.send_keys("Agnieszka")
+        el= self.driver.find_element_by_id(RegisterPageLocators.SURNAME_INPUT)
+        el.send_keys("Nowak")
+        el = self.driver.find_element_by_id(RegisterPageLocators.EMAIL_INPUT)
+        el.send_keys("anowak.com")
+        el = self.driver.find_element_by_id(RegisterPageLocators.USERNAME_INPUT)
+        el.send_keys("testernowak")
+        el = self.driver.find_element_by_id(RegisterPageLocators.PASSWORD_INPUT)
+        el.send_keys("passswwwo")
+        el = self.driver.find_element_by_class_name(RegisterPageLocators.SIGN_IN)
+        el.click()
 
-        # Assertion
-        #alert displays
-        assert self.driver.find_element_by_id('android:id/alertTitle').is_displayed() == True
-        #'email invalid' message displays
-        assert self.driver.find_element_by_id('android:id/message').text == 'Email invalid.'
+        # Assertions
+        el = self.driver.find_element_by_id(RegisterPageLocators.ALERT_WINDOW)
+        assert el.is_displayed() == True
+        el = self.driver.find_element_by_id(RegisterPageLocators.INVALID_EMAIL_MESSAGE)
+        assert el.text == 'Email invalid.'
 
-    def testSuccesfulLogin(self):
-        self.driver.is_app_installed('com.rememberthemilk.MobileRTM')
+    
+    def test2SuccesfulLogin(self):
+        self.driver.is_app_installed(GeneralLocators.REMEMBER_THE_MILK_APP)
         # click skip button if displayed
-        list = self.driver.find_elements_by_id('com.rememberthemilk.MobileRTM:id/alert_generic_notice')
-        if len(list)>0:
+        list = self.driver.find_elements_by_id(GeneralLocators.SKIPP_BUTTON)
+        if len(list) > 0:
             list[0].click()
         # click log in button
-        self.driver.find_element_by_id('com.rememberthemilk.MobileRTM:id/login_button').click()
-        self.driver.find_element_by_id('com.rememberthemilk.MobileRTM:id/username_field').send_keys('testeraga')
-        self.driver.find_element_by_id('com.rememberthemilk.MobileRTM:id/password_field').send_keys('Tester1234!')
-        self.driver.find_element_by_xpath('//*[@text="LOG IN"]').click()
-        sleep(1)
+        el = self.driver.find_element_by_id(LoginPageLocators.LOGIN_BUTTON)
+        el.click()
+        # username and password input
+        el = self.driver.find_element_by_id(LoginPageLocators.USERNAME_FIELD)
+        el.send_keys('testeraga')
+        el = self.driver.find_element_by_id(LoginPageLocators.PASSWORD_FIELD)
+        el.send_keys('Tester1234!')
+        el = self.driver.find_element_by_xpath(LoginPageLocators.LOG_IN)
+        el.click()
 
         #Assertion
-        # home page displays
-        assert self.driver.find_element_by_xpath('//*[@text="HOME"]').is_displayed() == True
+        el = self.driver.find_element_by_xpath(LoginPageLocators.HOME_TAB)
+        assert el.is_displayed() == True
+    '''
 
-        sleep(5)
+    def test3AddTask(self):
+        self.driver.is_app_installed(GeneralLocators.REMEMBER_THE_MILK_APP)
+        # click skip button if displayed
+        list = self.driver.find_elements_by_id(GeneralLocators.SKIPP_BUTTON)
+        if len(list) > 0:
+            list[0].click()
+        el = self.driver.find_element_by_id(LoginPageLocators.LOGIN_BUTTON)
+        el.click()
+        el = self.driver.find_element_by_id(LoginPageLocators.USERNAME_FIELD)
+        el.send_keys('testeraga')
+        el = self.driver.find_element_by_id(LoginPageLocators.PASSWORD_FIELD)
+        el.send_keys('Tester1234!')
+        el = self.driver.find_element_by_xpath(LoginPageLocators.LOG_IN)
+        el.click()
+        el = self.driver.find_element_by_xpath(HomePageLocators.STUDIA_TAB)
+        el.click()
+
+
 
 if __name__ == 'main':
-    suite = unittest.TestLoader().loadTestsFromTestCase(Test2Appium)
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestRememberTheMilk)
     unittest.TextTestRunner(verbosity=2).run(suite)
--- locators.py
+++ locators.py
@@ -1,4 +1,27 @@
+class GeneralLocators():
+    REMEMBER_THE_MILK_APP = 'com.rememberthemilk.MobileRTM'
+    SKIPP_BUTTON = "com.rememberthemilk.MobileRTM:id/alert_generic_notice"
+
 
 class RegisterPageLocators():
-    SKIPP_BUTTON = "com.rememberthemilk.MobileRTM:id/alert_generic_notice"
     SING_IN_BUTTON = "com.rememberthemilk.MobileRTM:id/sign_button"
+    NAME_INPUT = "com.rememberthemilk.MobileRTM:id/firstname_field"
+    SURNAME_INPUT = "com.rememberthemilk.MobileRTM:id/lastname_field"
+    EMAIL_INPUT = "com.rememberthemilk.MobileRTM:id/email_field"
+    USERNAME_INPUT = "com.rememberthemilk.MobileRTM:id/username_field"
+    PASSWORD_INPUT = "com.rememberthemilk.MobileRTM:id/password_field"
+    SIGN_IN = "android.widget.Button"
+    ALERT_WINDOW = 'android:id/alertTitle'
+    INVALID_EMAIL_MESSAGE = 'android:id/message'
+
+
+class LoginPageLocators():
+    LOGIN_BUTTON = "com.rememberthemilk.MobileRTM:id/login_button"
+    USERNAME_FIELD ="com.rememberthemilk.MobileRTM:id/username_field"
+    PASSWORD_FIELD = "com.rememberthemilk.MobileRTM:id/password_field"
+    LOG_IN = '//*[@text="LOG IN"]'
+    HOME_TAB = '//*[@text="HOME"]'
+
+
+class HomePageLocators():
+    STUDIA_TAB = '//android.widget.FrameLayout[@index="9"]'
