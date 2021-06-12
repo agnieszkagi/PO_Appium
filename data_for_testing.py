c1d50cd267bacf2eb27bff8404c02d8755566260
agnieszkagi <agnieszkagi@users.noreply.github.com>
2021-06-12 10:25:07 +0000

    Appium finished project

---
+++ data_for_testing.py
@@ -0,0 +1,10 @@
+class Data():
+    NAME = 'Agnieszka'
+    SURNAME = 'Nowak'
+    EMAIL = 'anowak'
+    USERNAME1 = 'testernowak'
+    USERNAME2 ='testeraga'
+    PASSWORD = 'Tester1234!'
+    TASK_TEXT = 'Send your CV'
+    ERROR_MESSAGE ='Email invalid.'
+
--- test_appium.py
+++ test_appium.py
@@ -6,6 +6,7 @@ from locators import GeneralLocators
 from locators import RegisterPageLocators
 from locators import LoginPageLocators
 from locators import HomePageLocators
+from data_for_testing import Data
 
 
 PATH = lambda p: os.path.abspath(
@@ -23,17 +24,17 @@ class TestRememberTheMilk(unittest.TestCase):
         desired_caps['appPackage'] = 'com.rememberthemilk.MobileRTM'
         desired_caps['appActivity'] = 'com.rememberthemilk.MobileRTM.Activities.RTMWelcomeActivity'
         desired_caps['noReset'] = 'true' #zeby zapisywalo zmiany
+        desired_caps['fullReset'] = 'false'
 
         # polaczenie z Appium
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
         self.driver.implicitly_wait(2)
 
-
     def tearDown(self):
         self.driver.quit()
 
-    '''
-    def test1RegistrationWithWronEmail(self):
+
+    def test1RegistrationWithWrongEmail(self):
         self.driver.is_app_installed(GeneralLocators.REMEMBER_THE_MILK_APP)
         #click skip button if displayed
         list = self.driver.find_elements_by_id(GeneralLocators.SKIPP_BUTTON)
@@ -45,15 +46,15 @@ class TestRememberTheMilk(unittest.TestCase):
         el.click()
         #Data input
         el = self.driver.find_element_by_id(RegisterPageLocators.NAME_INPUT)
-        el.send_keys("Agnieszka")
+        el.send_keys(Data.NAME)
         el= self.driver.find_element_by_id(RegisterPageLocators.SURNAME_INPUT)
-        el.send_keys("Nowak")
+        el.send_keys(Data.SURNAME)
         el = self.driver.find_element_by_id(RegisterPageLocators.EMAIL_INPUT)
-        el.send_keys("anowak.com")
+        el.send_keys(Data.EMAIL)
         el = self.driver.find_element_by_id(RegisterPageLocators.USERNAME_INPUT)
-        el.send_keys("testernowak")
+        el.send_keys(Data.USERNAME1)
         el = self.driver.find_element_by_id(RegisterPageLocators.PASSWORD_INPUT)
-        el.send_keys("passswwwo")
+        el.send_keys(Data.PASSWORD)
         el = self.driver.find_element_by_class_name(RegisterPageLocators.SIGN_IN)
         el.click()
 
@@ -61,7 +62,7 @@ class TestRememberTheMilk(unittest.TestCase):
         el = self.driver.find_element_by_id(RegisterPageLocators.ALERT_WINDOW)
         assert el.is_displayed() == True
         el = self.driver.find_element_by_id(RegisterPageLocators.INVALID_EMAIL_MESSAGE)
-        assert el.text == 'Email invalid.'
+        assert el.text == Data.ERROR_MESSAGE
 
     
     def test2SuccesfulLogin(self):
@@ -75,16 +76,16 @@ class TestRememberTheMilk(unittest.TestCase):
         el.click()
         # username and password input
         el = self.driver.find_element_by_id(LoginPageLocators.USERNAME_FIELD)
-        el.send_keys('testeraga')
+        el.send_keys(Data.USERNAME2)
         el = self.driver.find_element_by_id(LoginPageLocators.PASSWORD_FIELD)
-        el.send_keys('Tester1234!')
+        el.send_keys(Data.PASSWORD)
         el = self.driver.find_element_by_xpath(LoginPageLocators.LOG_IN)
         el.click()
 
         #Assertion
         el = self.driver.find_element_by_xpath(LoginPageLocators.HOME_TAB)
         assert el.is_displayed() == True
-    '''
+
 
     def test3AddTask(self):
         self.driver.is_app_installed(GeneralLocators.REMEMBER_THE_MILK_APP)
@@ -92,17 +93,29 @@ class TestRememberTheMilk(unittest.TestCase):
         list = self.driver.find_elements_by_id(GeneralLocators.SKIPP_BUTTON)
         if len(list) > 0:
             list[0].click()
+        # click log in button
         el = self.driver.find_element_by_id(LoginPageLocators.LOGIN_BUTTON)
         el.click()
+        # username and password input
         el = self.driver.find_element_by_id(LoginPageLocators.USERNAME_FIELD)
-        el.send_keys('testeraga')
+        el.send_keys(Data.USERNAME2)
         el = self.driver.find_element_by_id(LoginPageLocators.PASSWORD_FIELD)
-        el.send_keys('Tester1234!')
+        el.send_keys(Data.PASSWORD)
         el = self.driver.find_element_by_xpath(LoginPageLocators.LOG_IN)
         el.click()
-        el = self.driver.find_element_by_xpath(HomePageLocators.STUDIA_TAB)
+        # add task
+        el = self.driver.find_element_by_xpath(HomePageLocators.WORK_TAB)
+        el.click()
+        el = self.driver.find_element_by_xpath(HomePageLocators.ADD_BUTTON)
+        el.click()
+        el = self.driver.find_element_by_id(HomePageLocators.TASK_NAME)
+        el.send_keys(Data.TASK_TEXT)
+        el = self.driver.find_element_by_xpath(HomePageLocators.SAVE_BUTTON)
         el.click()
 
+        #Assertion
+        el = self.driver.find_element_by_xpath(HomePageLocators.TASK_TITLE)
+        assert el.is_displayed() == True
 
 
 if __name__ == 'main':
